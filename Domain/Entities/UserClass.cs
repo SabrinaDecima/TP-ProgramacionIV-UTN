e8fddf532e556fae25b5363ccc10c166a2d2f8d2
Sabrina Decima <sabrinadecia@gmail.com>
2025-09-23 19:46:58 -0300

    Commit prueba

--- Domain/Entities/UserClass.cs
+++ Domain/Entities/UserClass.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Domain.Entities
 {
     public class UserClass //tabla intermedia para relacion user y gymclass (muchos a muchos)
@@ -12,7 +7,6 @@ namespace Domain.Entities
         {
             UserId = userId;
             ClaseId = classId;
-
         }
         public int UserId { get; set; }
         public User? User { get; set; } //relacion con usuario. 
