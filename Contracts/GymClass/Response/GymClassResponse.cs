bfd733b96e4f74550ca766570d6fd7c02f28163f
Julian Meier <julianmeier@hotmail.com>
2025-09-26 19:10:04 -0300

    Introduce service contracts for User and GymClass entities

---
+++ Contracts/GymClass/Response/GymClassResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts.GymClass.Response
+{
+    public class GymClassResponse
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string InstructorNombre { get; set; }
+        public int DuracionMinutos { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
+    
--- Contracts/User/Response/UserResponse.cs
+++ Contracts/User/Response/UserResponse.cs
@@ -3,7 +3,15 @@
     public class UserResponse
     {
         public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Apellido { get; set; }
         public string Email { get; set; }
+
+        public string Telefono { get; set; }
+
+        public int PlanId { get; set; }
 
     }
 }
