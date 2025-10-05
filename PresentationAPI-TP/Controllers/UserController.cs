93d4f3544f99378c54226e89f97d621e2001b4f1
Julian Meier <julianmeier@hotmail.com>
2025-10-05 11:54:32 -0300

    Updating UserController

--- PresentationAPI-TP/Controllers/UserController.cs
+++ PresentationAPI-TP/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using Contracts.User.Response;
+﻿using Application.Interfaces;
+using Contracts.User.Request;
+using Contracts.User.Response;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationAPI_TP.Controllers
@@ -7,17 +9,79 @@ namespace PresentationAPI_TP.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+        private readonly IPlanService _planService;
+
+        public UserController(IUserService userService, IPlanService planService)
+        {
+            _userService = userService;
+            _planService = planService;
+        }
+
+        [HttpGet] 
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            
+            return Ok(users);
+        }
+
         [HttpGet("{id}")]
-        public ActionResult<UserResponse> GetUserById([FromRoute]int id)
+        public IActionResult GetById([FromRoute]int id)
         {
             
-            var fakeUser = new UserResponse
-            {
-                Id = id,
-                Email = "prueba@test.com"
-            };
+            var user = _userService.GetById(id);
 
-            return Ok(fakeUser);
+            if(user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+
+        public IActionResult Create([FromBody] CreateUserRequest request)
+        {
+            var result = _userService.CreateUser(request);
+
+            if (!result)
+            {
+                return BadRequest("No se pudo crear el usuario. Verifica completar los campos");
+
+            }
+
+            return Ok("Usuario creado correctamente");
+        }
+
+        [HttpPut("{id}")]
+
+        public IActionResult Update([FromRoute]int id, [FromBody] UpdateUserRequest request)
+        {
+            var plan = _planService.GetById(request.PlanId);
+
+            if(plan == null)
+            {
+                return BadRequest("El planId proporcionado no es valido");
+            }
+
+            var user = _userService.GetById(id);
+
+
+            if(user == null)
+            {
+                return NotFound("Usuario no encontrado");
+            }
+
+            var updatedUser = _userService.UpdateUser(id, request);
+
+            if (!updatedUser)
+            {
+                return NotFound("No se pudo actualizar el usuario");
+            }
+
+            return Ok("Usuario actualizado");
         }
     }
 }
