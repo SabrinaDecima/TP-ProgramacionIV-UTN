0aa3089b446b2e8d1bb53d5e78cdbfd2e682a13d
Sabrina Decima <sabrinadecia@gmail.com>
2025-10-19 11:57:19 -0300

    Modified Policies in HHTPClient

--- PresentationAPI-TP/Program.cs
+++ PresentationAPI-TP/Program.cs
@@ -1,6 +1,8 @@
 using Application;
 using Infrastructure;
+using Polly;
 using PresentationAPI_TP;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +11,22 @@ builder.Services
     .AddApplication()
     .AddInfrastructure(builder);
 
+builder.Services.AddHttpClient("MercadoPago", client =>
+{
+    client.BaseAddress = new Uri("https://api.mercadopago.com/");
+})
+.AddPolicyHandler(Policy
+    .Handle<HttpRequestException>()
+    .OrResult<HttpResponseMessage>(r => !r.IsSuccessStatusCode)
+    .WaitAndRetryAsync(
+        retryCount: 3,
+        sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt))
+     
+    ));
+
+
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -16,6 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 
 app.UseHttpsRedirection();
 
