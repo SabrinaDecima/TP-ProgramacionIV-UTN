3ffcde3a1cf8bc8dcaa774eeff7102b3847ddcd1
martin flecha <martin248f@gmail.com>
2025-11-05 19:44:38 -0300

    enable open CORS policy

--- PresentationAPI-TP/Program.cs
+++ PresentationAPI-TP/Program.cs
@@ -36,6 +36,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthentication();
 
 app.UseAuthorization();
